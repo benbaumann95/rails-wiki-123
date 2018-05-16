ae37ed9c63b753972101de3d27096ee15cff3a7a
Ben Baumann <benbaumann95@hotmail.co.uk>
2018-05-16 12:22:09 +0100

    rails wiki

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Article.destroy_all
+
+def fake_content
+  paragraphs = []
+  paragraphs << Faker::Beer.style
+  paragraphs << Faker::Beer.hop
+  paragraphs << Faker::Beer.yeast
+  paragraphs << Faker::Beer.malts
+  paragraphs << Faker::Beer.alcohol
+  paragraphs.flatten.join("\n\n")
+end
+
+20.times do
+  Article.create(
+    title: Faker::Beer.name,
+    content: fake_content
+  )
+end
